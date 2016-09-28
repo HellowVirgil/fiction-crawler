4c51cb6b82195c940ec565ec23a519170556cb4a
Virgil <1135895008@qq.com>
2016-09-28 16:55:00 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,13 +2,13 @@ var fs = require('fs');
 var debug = require('debug')('crawler');
 var mkdirp = require('mkdirp');
 
-exports.write_chapter = function(book_path, chapter, content){
+exports.write_chapter = function(book_path, chapter, content, timeStamp){
   content = content.replace('[笔趣库手机版 m.biquku.com]', '');
-  var timeStamp = new Date();
+
   fs.writeFile('dist/' + book_path + chapter.num + '.html', content, function (err) {
     if (err) throw err;
     debug('It\'s saved!');
-    console.log(chapter.title + ' 抓取完成！耗时' + (new Date() - timeStamp) + 'ms');
+    console.log(chapter.title + ' 抓取完成！累计耗时' + (new Date() - timeStamp) + 'ms');
   });
 }
 
